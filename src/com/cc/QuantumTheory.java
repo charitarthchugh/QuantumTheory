3987d8e71004d1f5a712e0d8b55c72ec6314539f
Charitarth <charitarth.chugh@gmail.com>
2019-11-27 21:35:04 -0500

    Improve code and add wavelength functionality.
    I need to revise the frequency code to be more like the wavelength code, to make it better for the user

--- src/com/cc/QuantumTheory.java
+++ src/com/cc/QuantumTheory.java
@@ -7,12 +7,15 @@ public class QuantumTheory {
     static final char wave = '\u03BB' ;
 
     public static void main(String[] args) {
+        //declaring all variables and objects
         Scanner scan = new Scanner(System.in);
-        boolean passthrough = true;
+        boolean passthrough = true,force=true;
+        String calculate,wavelength,energy;
+        double waveL,enerG,freq;
         System.out.println("Hello.\nThis is a program that determines the wavelength, energy, or frequency of a wave of light, given some inputs.");
         do {
             System.out.println("What do you want to calculate?");
-            String calculate = scan.nextLine();
+            calculate = scan.nextLine();
             System.out.println("We are now calculating.... " + whatCalc(calculate));
             if (whatCalc(calculate).equals("\nIncorrect input. Choose between \"frequency\", \"wavelength\", or \"energy\" ")) {
                 System.out.println("What do you want to calculate?");
@@ -20,10 +23,8 @@ public class QuantumTheory {
             } else if (whatCalc(calculate).equals("quit")) {
                 passthrough = false;
             } else if (whatCalc(calculate).equals("frequency")) {
-                System.out.println("Now type in known values.If not available, leave field blank");
-                String wavelength, energy;
-                boolean force = true;
-                double waveL,enerG;
+                //I need to revise this structure so that it finds what known vars are being entered
+                System.out.println("Now type in known values,in meters/Joules(hint- just like a calculator, e). \n If not available, leave field blank.");
                 System.out.print(wave + ": ");
                 wavelength = scan.nextLine();
                 System.out.print("E: ");
@@ -32,6 +33,7 @@ public class QuantumTheory {
                     while (force) {
                         try {
                             waveL = Double.parseDouble(wavelength);
+                            System.out.println("The frequency is:" + frequency(waveL));
                             force = false;
                         } catch (Exception e) {
                             System.out.println("Enter numbers only please");
@@ -39,12 +41,13 @@ public class QuantumTheory {
                             wavelength = scan.nextLine();
                         }
                     }
-                    System.out.println(frequency(waveL));
+
                 }
                 else if (!energy.isEmpty() && wavelength.isEmpty()) {
                     while (force) {
                         try {
                             enerG = Double.parseDouble(wavelength);
+                            System.out.println("The frequency is: "+frequentEnergy(enerG) +"meters");
                             force = false;
                         } catch (Exception e) {
                             System.out.println("Enter numbers other than only please");
@@ -52,12 +55,45 @@ public class QuantumTheory {
                             energy = scan.nextLine();
                         }
                     }
-                    System.out.println(frequentEnergy(enerG));
                 }
             }
+            else if (whatCalc(calculate).equals("wavelength")){
+                System.out.println("What are the input types?(Either \"energy\" or \"frequency\")");
+                calculate=scan.nextLine();
+                if (calculate.equalsIgnoreCase("energy")){
+                    System.out.println("Input energy(hint- just like a calculator, e for scientific notation)");
+                    while(force)
+                        System.out.print("E: ");
+                    try {
+                        enerG=scan.nextDouble();
+                        force=false;
+                        System.out.println(wave+"= "+ waveEnergy(enerG));
+                    }catch (Exception e){
+                        System.out.println("Enter numbers only please");
+                        System.out.print("E: ");
+                        enerG = scan.nextDouble();
+                    }
+                }
+                else if (calculate.equalsIgnoreCase("frequency")){
+                    System.out.println("Input frequency(hint- just like a calculator, e for scientific notation)");
+                    while(force)
+                        System.out.print("s^-1: ");
+                        try {
+                            enerG=scan.nextDouble();
+                            force=false;
+                            System.out.println(wave+"= "+ waveEnergy(enerG));
+                        }catch (Exception e){
+                            System.out.println("\nEnter numbers only please");
+                            System.out.print("E: ");
+                            enerG = scan.nextDouble();
+                    }
+                }
+            }
+                //System.out.println("Now type in known values, in s^-1 or Joules(hint- just like a calculator, e).\n Leave values blank if not available");
 
 
-        }
+            }
+
         while (passthrough);
     }
     // this method determines what the use wants to calculate
@@ -87,6 +123,9 @@ public class QuantumTheory {
         return wavelength;
     }
     //Method to get frequency from wavelength
+    public static double waveFreq(double frequency){
+        return C/frequency;
+    }
     public static double frequency(double wavelength){
         /*
         *C=wavelength*frequency
